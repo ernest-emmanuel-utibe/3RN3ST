e1395615c2319242e75564e00280f74b57fe7afb
Ernest <em123nest@gmail.com>
2023-02-06 08:49:54 +0100

    updated commit

---
+++ src/selfPractice/MagicNumberGuess.java
@@ -0,0 +1,27 @@
+package selfPractice;
+
+import java.util.Scanner;
+
+public class MagicNumberGuess {
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 101);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the magic number form 0 - 100");
+
+        int guess = -1;
+        while(guess != number) {
+            System.out.println("Enter your guess");
+            guess = input.nextInt();
+
+            if (guess == number) {
+                System.out.println("Yes, the number is " + number);
+            } else if (guess > number) {
+                System.out.println("Your guess is too high");
+            }
+            else {
+                System.out.println("Your guess is too low");
+            }
+        }
+    }
+}
---
+++ src/selfPractice/RepeatAndAddQuiz.java
@@ -0,0 +1,22 @@
+package selfPractice;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RepeatAndAddQuiz {
+    public static void main(String[] args) throws IOException {
+        int number1 = (int) (Math.random() * 10);
+        int number2 = (int) (Math.random() * 10);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is " + number1 + " + " + number2 + " ? ");
+        int answer = input.nextInt();
+
+        while (number1 + number2 != answer) {
+            System.out.print("Wrong answer. Try again.  What is " + number1 + " + " + number2 + " ? ");
+            answer = input.nextInt();
+        }
+            System.out.println("Correct answer. You got it right, cheers mate.");
+    }
+}
