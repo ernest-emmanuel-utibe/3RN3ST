b3e278fe811efc6bf10458688969dd85c28f6406
Ernest <em123nest@gmail.com>
2023-02-12 03:09:57 +0100

    updated commit

---
+++ src/loops/arraysLoops/Break.java
@@ -0,0 +1,27 @@
+package loops.arraysLoops;
+
+public class Break {
+    public static void main(String[] args) {
+        int[] arrayOfInts =
+                { 32, 87, 3, 589,
+                        12, 1076, 2000,
+                        8, 622, 127 };
+        int searchfor = 12;
+
+        int i;
+        boolean foundIt = false;
+
+        for (i = 0; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] == searchfor) {
+                foundIt = true;
+                break;
+            }
+        }
+
+        if (foundIt) {
+            System.out.println("Found " + searchfor + " at index " + i);
+        } else {
+            System.out.println(searchfor + " not in the array");
+        }
+    }
+}
---
+++ src/loops/switch_statements/FallThrough.java
@@ -0,0 +1,36 @@
+package loops.switch_statements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FallThrough {
+    public static void main(String[] args) {
+        List<String> futureMonths = new ArrayList<String>();
+        int month = 8;
+
+        switch (month) {
+            case 1:  futureMonths.add("January");
+            case 2:  futureMonths.add("February");
+            case 3:  futureMonths.add("March");
+            case 4:  futureMonths.add("April");
+            case 5:  futureMonths.add("May");
+            case 6:  futureMonths.add("June");
+            case 7:  futureMonths.add("July");
+            case 8:  futureMonths.add("August");
+            case 9:  futureMonths.add("September");
+            case 10: futureMonths.add("October");
+            case 11: futureMonths.add("November");
+            case 12: futureMonths.add("December");
+                break;
+            default: break;
+        }
+
+        if (futureMonths.isEmpty()) {
+            System.out.println("Invalid month number");
+        } else {
+            for (String monthName : futureMonths) {
+                System.out.println(monthName);
+            }
+        }
+    }
+}
