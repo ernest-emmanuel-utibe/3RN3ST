e324f6e114819979764d05056d69315991a3b76b
Ernest <em123nest@gmail.com>
2023-02-12 02:24:43 +0100

    updated commit

---
+++ src/loops/ifStatements/Comparison.java
@@ -0,0 +1,18 @@
+package loops.ifStatements;
+
+public class Comparison {
+    public static void main(String[] args) {
+        int value1 = 1;
+        int value2 = 2;
+        if(value1 == value2)
+            System.out.println("value1 == value2");
+        if(value1 != value2)
+            System.out.println("value1 != value2");
+        if(value1 > value2)
+            System.out.println("value1 > value2");
+        if(value1 < value2)
+            System.out.println("value1 < value2");
+        if(value1 <= value2)
+            System.out.println("value1 <= value2");
+    }
+}
