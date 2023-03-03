41a98a207c47e583d8cf3d2021b8c62dd79aa4c8
Ernest <em123nest@gmail.com>
2023-03-03 01:00:43 +0100

    updated commit

---
+++ src/advance_java/CounterDemo.java
@@ -0,0 +1,12 @@
+package advance_java;
+
+import org.w3c.dom.css.Counter;
+
+import java.util.Arrays;
+
+public class CounterDemo {
+    public static void main(String[] args) {
+        String[] strings = new String[]{"Words", "Counters", "Numbers"};
+        System.out.println(Arrays.toString(strings).toUpperCase());
+    }
+}
