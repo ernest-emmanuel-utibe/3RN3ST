7a22eaa4a9598227247d507be3c8964b6bbebf01
Ernest <em123nest@gmail.com>
2023-03-17 08:40:30 +0100

    updated commit

--- src/array/ReverseArray.java
+++ src/array/ReverseArray.java
@@ -2,20 +2,10 @@ package array;
 
 public class ReverseArray {
     public int[] reverseAnArray(int[] number) {
-        int[] x = new int[6];
-        int temp = 0;
-
-        for (int i = 0; i < x.length / 2; i++) {
-            temp = x[i];
-            x[i] = x[x.length - i - 1];
-            x[x.length - i -1] = temp;
-        }
-
-        for (int j = x.length -1; j >= 0; j--) {
-            x[j] = x[x.length - j - 1];
-            x[x.length - j -1] = temp;
+        int[] x = new int[number.length];
+        for (int i = 0; i < x.length; i++) {
+            x[i] = number[x.length - i - 1];
         }
         return x;
-
     }
 }
